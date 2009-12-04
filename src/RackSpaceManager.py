aa534d82a5518113042775e9a76b383d815eb7db
Sergey <psi@psiholazha.(none)>
2009-12-04 16:32:06 +0300

    Added empty RackSpaceManager

---
+++ src/RackSpaceManager.py
@@ -0,0 +1,9 @@
+from RackSpaceClient import RackSpaceClient
+
+class RackSpaceManager:
+    def __init__(self, user = '', key = ''):
+    def ListServers(self, isDetail = False):
+    def ListImages(self):
+
+
+
