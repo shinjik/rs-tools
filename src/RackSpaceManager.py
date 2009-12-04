158ca64af512c26878fa409c2292e512bcc7d499
Sergey <psi@psiholazha.(none)>
2009-12-04 19:01:30 +0300

    Added server, ip, and state objects, server list now returns list of objects, added initial version of rs-server-list script

--- src/RackSpaceManager.py
+++ src/RackSpaceManager.py
@@ -1,6 +1,35 @@
 from RackSpaceClient import RackSpaceClient
 import simplejson
 
+class IP:
+    def __init__(self):
+        self.ip = ""
+        self.isPublic = False
+
+class State:
+    def __init__(self):
+        self.status = ""
+        self.progress = 0
+
+class Server:
+    def __init__(self):
+        # Server id
+        self.id = ""
+
+        # Server name
+        self.name = ""
+
+        # Additional description
+        self.description = ""
+
+        # List of ip addresses
+        self.ip = []
+
+        # Server state
+        self.state = State()
+
+
+
 class RackSpaceManager:
 # TODO: add exception handling
     def __init__(self, user = '', key = ''):
@@ -12,7 +41,31 @@ class RackSpaceManager:
         else:
             method = "/servers"
         servers = self.rsClient.SendRequest(rType = "GET", method = method, data = None, params = None )
-        return simplejson.loads(servers['body'])
+        serverList = []
+        for server in simplejson.loads(servers['body'])['servers']:
+            srv = Server()
+            srv.id = server['id']
+            srv.name = server['name']
+            if isDetail:
+                state = State()
+                state.status = server['status']
+                state.progress = server['progress']
+                srv.state = state
+                # parse IP list
+                def parse_ips(addresses, isPublic):
+                    res = []
+                    for addr in addresses:
+                        s_addr = IP()
+                        s_addr.ip = addr
+                        s_addr.isPublic = isPublic
+                        res.append(s_addr)
+                    return res
+                srv.ip.append(parse_ips(server['addresses']['public'], True))
+                srv.ip.append(parse_ips(server['addresses']['private'], False))
+            serverList.append(srv)
+        return serverList
+
+
 
     def ListImages(self, isDetail = False):
         if isDetail:
