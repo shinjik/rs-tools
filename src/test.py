7ffc09b8c8f6ebe49d52dd682b02eb45599710a3
shinjik <psi@igni.ru>
2009-12-04 02:33:57 -0700

    deleted fake data

--- src/test.py
+++ src/test.py
@@ -1,7 +1,6 @@
-
 from Authentication import Authentication
 
-auth = Authentication(user='kishanov', key='a7a97224659b2d68a7c3efe969583744')
+auth = Authentication(user='', key='')
 auth.doLogin()
 print "auth successful, management url is: ['%s'], api key is " % auth.getServerManagementURL(), auth.getAuthToken()
 
