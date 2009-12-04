ec1591d442ca7451ab8d47f4d35ee0ecb1a0add9
Eugene Suchkov <esuchkov@esuchkov.ws.carina.griddynamics.net>
2009-12-04 16:41:58 +0300

    RackSpaceManager quickstart .gitignore added

--- src/RackSpaceManager.py
+++ src/RackSpaceManager.py
@@ -2,8 +2,10 @@ from RackSpaceClient import RackSpaceClient
 
 class RackSpaceManager:
     def __init__(self, user = '', key = ''):
+        self.rsClient = RackSpaceClient(None, None)
     def ListServers(self, isDetail = False):
     def ListImages(self):
-
+        images = self.rsClient.SendRequest(rType = "GET", method = "images", params = None )
+        return images
 
 
