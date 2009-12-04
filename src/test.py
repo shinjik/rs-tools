34c1a1e3d624e0c163604299e9d74ead10ccbc78
Sergey <psi@psiholazha.(none)>
2009-12-04 14:54:33 +0300

    Removed test.py

--- src/test.py
+++
@@ -1,8 +0,0 @@
-from Authentication import Authentication
-
-auth = Authentication(user='', key='')
-auth.doLogin()
-print "auth successful, management url is: ['%s'], api key is " % auth.getServerManagementURL(), auth.getAuthToken()
-
-
-
