0bdd7a58c92eb370bdbb7fd752c411a6bde28c13
Sergey <psi@psiholazha.(none)>
2009-12-07 19:32:14 +0300

    Added CreateServer method and rs-server-add script

--- src/RackSpaceManager.py
+++ src/RackSpaceManager.py
@@ -1,4 +1,5 @@
 from RackSpaceClient import RackSpaceClient
+from Exceptions import *
 import simplejson
 
 class IP:
@@ -28,6 +29,9 @@ class Server:
         # Server state
         self.state = State()
 
+        # Root password
+        self.rootPassword = ""
+
 class ServerImage:
     def __init__(self):
         self.id = ""
@@ -40,9 +44,32 @@ class ServerImage:
 
 def extractValueOrNone(key, inputDict):
     if isinstance(inputDict, dict):
-        if key in inputDict.keys():
-            return inputDict[key]
+        return inputDict.get(key, None)
     return None
+
+def parseServer(server, isDetail = False, isPassword = False):
+    srv = Server()
+    srv.id = server['id']
+    srv.name = server['name']
+    if isDetail:
+        state = State()
+        state.status = server.get('status', None)
+        state.progress = server.get('progress', None)
+        srv.state = state
+        # parse IP list
+        def parse_ips(addresses, isPublic):
+            res = []
+            for addr in addresses:
+                s_addr = IP()
+                s_addr.ip = addr
+                s_addr.isPublic = isPublic
+                res.append(s_addr)
+                return res
+        srv.ip.append(parse_ips(server['addresses']['public'], True))
+        srv.ip.append(parse_ips(server['addresses']['private'], False))
+        if isPassword:
+            srv.rootPassword = server['adminPass']
+    return srv
 
 
 class RackSpaceManager:
@@ -55,28 +82,10 @@ class RackSpaceManager:
             method = "/servers/detail"
         else:
             method = "/servers"
-        servers = self.rsClient.SendRequest(rType = "GET", method = method, data = None, params = None )
+        servers = self.rsClient.SendRequest(rType = "GET", method = method, data = None, params = None)
         serverList = []
         for server in simplejson.loads(servers['body'])['servers']:
-            srv = Server()
-            srv.id = server['id']
-            srv.name = server['name']
-            if isDetail:
-                state = State()
-                state.status = server['status']
-                state.progress = server['progress']
-                srv.state = state
-                # parse IP list
-                def parse_ips(addresses, isPublic):
-                    res = []
-                    for addr in addresses:
-                        s_addr = IP()
-                        s_addr.ip = addr
-                        s_addr.isPublic = isPublic
-                        res.append(s_addr)
-                    return res
-                srv.ip.append(parse_ips(server['addresses']['public'], True))
-                srv.ip.append(parse_ips(server['addresses']['private'], False))
+            srv = parseServer(server, isDetail)
             serverList.append(srv)
         return serverList
 
@@ -87,7 +96,7 @@ class RackSpaceManager:
             method = "/images/detail"
         else:
             method = "/images"
-        images = self.rsClient.SendRequest(rType = "GET", method = method, data = None, params = None )
+        images = self.rsClient.SendRequest(rType = "GET", method = method, data = None, params = None)
         imageList = []
         for img in simplejson.loads(images['body'])['images']:
             image = ServerImage()
@@ -100,6 +109,41 @@ class RackSpaceManager:
             image.updated = extractValueOrNone("updated", img)
             imageList.append(image)
         return imageList
+
+    def CreateServer(self, name, imageId, flavorId, metadata = None, personality = None):
+        # Creates server, returns new object or throws exception if something wrong.
+
+        # constructing request
+        srv = {"name": name, "imageId": imageId, "flavorId": flavorId}
+        if metadata is not None:
+            srv["metadata"] = metadata
+        if personality is not None:
+            srv["personality"] = personality
+
+        req = simplejson.dumps({"server": srv})
+
+        # sending
+        try:
+            response = self.rsClient.SendRequest(rType="POST", method = "/servers", data = req, params = None)
+        except RackSpaceException:
+            raise
+        # parsing response
+# TODO: parse response, handle errors
+        if response["code"] == 202:
+            server = parseServer(simplejson.loads(response['body'])['server'], True, True)
+            return server
+        raise RackSpaceException()
+
+
+    def DeleteServer(self, serverId):
+        # Delete specified by id server
+        try:
+            response = self.rsClient.SendRequest(rType="DELETE", method = "/servers/"+serverId, data = None, params = None)
+        except RackSpaceException:
+            raise
+        if response["code"] != 202
+            # Something wrong
+            raise RackSpaceException()
 
 
 
