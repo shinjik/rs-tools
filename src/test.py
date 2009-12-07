408c7db667a55b86aa5fd37db3574c2c326b2ee8
Eugene Suchkov <esuchkov@griddynamics.com>
2009-12-07 19:26:03 +0300

    automapper prototype

--- src/test.py
+++ src/test.py
@@ -1,6 +1,7 @@
 from RackSpaceManager import RackSpaceManager
+from pprint import pprint
 
 #client = RackSpaceClient(user = '', key = '')
 manager = RackSpaceManager()
-print manager.ListServers(isDetail = True)
-print manager.ListImages(isDetail = True)
+pprint (manager.ListServers(isDetail = True))
+pprint (manager.ListImages(isDetail = True))
