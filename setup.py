7383298cc513a0e1b1858f9f646ec0a6f2a61d52
Sergey <psi@psiholazha.(none)>
2010-04-09 16:40:08 +0400

    corrected dir structure and setup script

--- setup.py
+++ setup.py
@@ -9,6 +9,13 @@ setup(
         author = "Sergey Plastinkin",
         author_email = "splastinkin@griddynamics.com",
         url="http://github.com/shinjik/rs-tools/tree",
-        py_modules = ["RackSpaceClient", "RackSpaceManager"]
+        #py_modules = ["RackSpaceClient", "RackSpaceManager"],
+        packages = ["rstools"],
+        package_dir = {'rstools': 'src'},
+
+        scripts = ["scripts/rs-image-delete", "scripts/rs-image-list", "scripts/rs-image-save", 
+            "scripts/rs-server-add", "scripts/rs-server-delete", "scripts/rs-server-reboot", 
+            "scripts/rs-server-list", "scripts/rs-server-update", "scripts/rs-server-rebuild", "scripts/rs-flavor-list"]
+
 )
 
--- src/test.py
+++
@@ -1,7 +0,0 @@
-from RackSpaceManager import RackSpaceManager
-from pprint import pprint
-
-#client = RackSpaceClient(user = '', key = '')
-manager = RackSpaceManager()
-pprint (manager.ListServers(isDetail = True))
-pprint (manager.ListImages(isDetail = True))
